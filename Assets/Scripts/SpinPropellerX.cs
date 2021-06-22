90d9875789839f20fa52911bcaa0ffa74706aa2a
sangji0729 <85105404+sangji0729@users.noreply.github.com>
2021-06-22 18:05:47 +0900

    d
    d

---
+++ Assets/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed*Time.deltaTime,Space.Self);
+    }
+}
